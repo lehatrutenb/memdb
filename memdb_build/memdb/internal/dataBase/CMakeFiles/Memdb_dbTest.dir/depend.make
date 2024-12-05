b51c33ab4c6c23f30e718eacbcb06f0a9e058a4d
lehatrutenb <lehatrutenb@gmail.com>
2024-12-06 00:55:58 +0300

    finally rm build dir

--- memdb_build/memdb/internal/dataBase/CMakeFiles/Memdb_dbTest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Memdb_dbTest.
-# This may be replaced when dependencies are built.
--- memdb_build/memdb/internal/type/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lehatrutenb/Desktop/prog/memdb/memdb/internal/type
-# Build directory: /Users/lehatrutenb/Desktop/prog/memdb/memdb_build/memdb/internal/type
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
